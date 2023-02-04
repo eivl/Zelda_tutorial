750c51636c8a0af027cc48160aa2ecca72a3d56c
Eivind Teig <eivind.teig@gmail.com>
2023-02-05 00:52:08 +0100

    Add floor as level surface.

--- level.py
+++ level.py
@@ -1,3 +1,5 @@
+import operator
+
 import pygame
 from settings import *
 from tile import Tile
@@ -42,10 +44,14 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_height = self.display_surface.get_height() // 2
         self.offset = pygame.math.Vector2()
 
+        # Create the floor.
+        self.floor_surface = pygame.Surface(pygame.image.load("graphics/test/floor.png").convert())
+        self.floor_rect = self.floor_surface.get_rect(topleft=(0, 0))
+
     def custom_draw(self, player):
         self.offset.x = self.half_width - player.rect.centerx
         self.offset.y = self.half_height - player.rect.centery
 
-        for sprite in self.sprites():
+        for sprite in sorted(self.sprites(), key=operator.attrgetter("rect.y")):
             offset_pos = sprite.rect.topleft + self.offset
             self.display_surface.blit(sprite.image, offset_pos)
